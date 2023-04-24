be899dfcb60eae69e8557f8d39ae852173df5c72
Tyler Holhubner <tholhubner@gmail.com>
2023-04-24 16:00:38 -0400

    added correct value to last lap label. Changed color of value field to 1111 instead of 0001

--- ACTTApp.py
+++ ACTTApp.py
@@ -33,9 +33,9 @@ def acMain(ac_version):
 	ac.log("Hello from AC TT!")
 
 	l_lapcount = ac.addLabel(appWindow, "Laps: 0")
-	l_lastlaptime = ac.addLabel(appWindow, "Last Lap: ")
+	l_lastlaptime = ac.addLabel(appWindow, "Last Lap: N/A")
 	ac.setPosition(l_lapcount, 3, 30)
-	ac.setPosition(l_lastlaptime, 3, 30)
+	ac.setPosition(l_lastlaptime, 3, 50)
 	return "AC TT App"
 
 def acUpdate(deltaT):
@@ -51,8 +51,8 @@ def acUpdate(deltaT):
 		if lapInvalidated == False:
 			lastLapTime = lastLap
 			ac.log("{} last lap in MS".format(str(lastLapTime)))
-			ac.setText(l_lastlaptime, "Last Lap: Butthole")
-			ac.setFontColor(l_lastlaptime, 0, 0, 0, 1)
+			ac.setText(l_lastlaptime, "Last Lap: {}".format(str(lastLapTime)))
+			ac.setFontColor(l_lastlaptime, 1, 1, 1, 1)
 			validLaps.append(lastLapTime)
 			ac.log("{} valid laps array".format(str(validLaps)))
 		else:
