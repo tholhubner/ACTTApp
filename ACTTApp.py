64f5151eeee6b1da25c4b108a81563fa8673c4ca
Tyler Holhubner <tholhubner@gmail.com>
2023-04-23 22:39:30 -0400

    trying to add a longer delay to the app

--- ACTTApp.py
+++ ACTTApp.py
@@ -7,6 +7,7 @@ l_lastlaptime=0
 lapcount=0
 lastLapTime=0
 validLaps=[]
+time=0
 
 def acMain(ac_version):
 	global l_lapcount
@@ -24,25 +25,31 @@ def acMain(ac_version):
 	return "ACTTApp"
 
 def acUpdate(deltaT):
-	global l_lapcount, l_lastlaptime, lapcount, lastLapTime
-	laps = ac.getCarState(0, acsys.CS.LapCount)
-	lastLap = ac.getCarState(0, acsys.CS.LastLap)
-	ac.log("{} spline postion test".format(str(ac.getCarState(0, acsys.CS.NormalizedSplinePosition))))
-	if laps > lapcount:
-		lapcount = laps
-		ac.log("{} laps completed".format(lapcount))
-		ac.setText(l_lapcount, "Laps: {}".format(lapcount))
-		lapInvalidated = ac.getCarState(0, acsys.CS.LapInvalidated)
-		ac.log("{} lap validation value RAW".format(str(lapInvalidated)))
-		if not lapInvalidated:
-			lastLapTime = lastLap
-			ac.log("{} last lap in MS".format(str(lastLapTime)))
-			ac.setText(l_lastlaptime, "Last Lap: {}".format(str(lastLapTime)))
-			ac.setFontColor(l_lastlaptime, 0, 0, 0, 1)
-			validLaps.append(lastLapTime)
-			ac.log("{} valid laps array".format(str(validLaps)))
-		else:
-			lastLapTime = 0
-			ac.log("Last Lap was invalid")
-			ac.setText(l_lastlaptime, "Last Lap: Invalid Lap")
-			ac.setFontColor(l_lastlaptime, 1, 0, 0, 1)
+	global time, l_lapcount, l_lastlaptime, lapcount, lastLapTime
+	time += deltaT
+	if (time > 1):
+		laps = ac.getCarState(0, acsys.CS.LapCount)
+		lastLap = ac.getCarState(0, acsys.CS.LastLap)
+		ac.log("{} spline postion test".format(str(ac.getCarState(0, acsys.CS.NormalizedSplinePosition))))
+		if laps > lapcount:
+			lapcount = laps
+			ac.log("{} laps completed".format(lapcount))
+			ac.setText(l_lapcount, "Laps: {}".format(lapcount))
+			lapInvalidated = ac.getCarState(0, acsys.CS.LapInvalidated)
+			ac.log("{} lap validation value RAW".format(str(lapInvalidated)))
+			if not lapInvalidated:
+				lastLapTime = lastLap
+				ac.log("{} last lap in MS".format(str(lastLapTime)))
+				ac.setText(l_lastlaptime, "Last Lap: {}".format(str(lastLapTime)))
+				ac.setFontColor(l_lastlaptime, 0, 0, 0, 1)
+				validLaps.append(lastLapTime)
+				ac.log("{} valid laps array".format(str(validLaps)))
+				time = 0
+			else:
+				lastLapTime = 0
+				ac.log("Last Lap was invalid")
+				ac.setText(l_lastlaptime, "Last Lap: Invalid Lap")
+				ac.setFontColor(l_lastlaptime, 1, 0, 0, 1)
+				time = 0
+	else:
+		time = 0
