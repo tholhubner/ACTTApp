0d61cfba5934fb35d10e820987fdb752277295e6
Tyler Holhubner <tholhubner@gmail.com>
2023-04-23 23:33:01 -0400

    changed app name. updated timer to only effect my logging, and got more specific on the value I want from info.physics

--- ACTTApp.py
+++ ACTTApp.py
@@ -16,7 +16,7 @@ time=0
 def acMain(ac_version):
 	global l_lapcount
 
-	appWindow = ac.newApp("ACTTApp")
+	appWindow = ac.newApp("AC TT App")
 	ac.setSize(appWindow, 200, 200)
 
 	ac.log("Hello, Assetto Corsa application world!")
@@ -26,31 +26,31 @@ def acMain(ac_version):
 	l_lastlaptime = ac.addLabel(appWindow, "Last Lap: N/A")
 	ac.setPosition(l_lapcount, 3, 30)
 	ac.setPosition(l_lastlaptime, 3, 50)
-	return "ACTTApp"
+	return "AC TT App"
 
 def acUpdate(deltaT):
 	global time, l_lapcount, l_lastlaptime, lapcount, lastLapTime
+	laps = ac.getCarState(0, acsys.CS.LapCount)
+	lastLap = ac.getCarState(0, acsys.CS.LastLap)
 	time += deltaT
-	if time > 10:
-		ac.log("{} testing Info".format(str(info.physics)))
-		laps = ac.getCarState(0, acsys.CS.LapCount)
-		lastLap = ac.getCarState(0, acsys.CS.LastLap)
+	if time > 2:
+		ac.log("{} testing Info".format(str(info.physics.numberOfTyresOut)))
 		time = 0
-		if laps > lapcount:
-			lapcount = laps
-			ac.log("{} laps completed".format(lapcount))
-			ac.setText(l_lapcount, "Laps: {}".format(lapcount))
-			lapInvalidated = ac.getCarState(0, acsys.CS.LapInvalidated)
-			ac.log("{} lap validation value RAW".format(str(lapInvalidated)))
-			if not lapInvalidated:
-				lastLapTime = lastLap
-				ac.log("{} last lap in MS".format(str(lastLapTime)))
-				ac.setText(l_lastlaptime, "Last Lap: {}".format(str(lastLapTime)))
-				ac.setFontColor(l_lastlaptime, 0, 0, 0, 1)
-				validLaps.append(lastLapTime)
-				ac.log("{} valid laps array".format(str(validLaps)))
-			else:
-				lastLapTime = 0
-				ac.log("Last Lap was invalid")
-				ac.setText(l_lastlaptime, "Last Lap: Invalid Lap")
-				ac.setFontColor(l_lastlaptime, 1, 0, 0, 1)
+	if laps > lapcount:
+		lapcount = laps
+		ac.log("{} laps completed".format(lapcount))
+		ac.setText(l_lapcount, "Laps: {}".format(lapcount))
+		lapInvalidated = ac.getCarState(0, acsys.CS.LapInvalidated)
+		ac.log("{} lap validation value RAW".format(str(lapInvalidated)))
+		if not lapInvalidated:
+			lastLapTime = lastLap
+			ac.log("{} last lap in MS".format(str(lastLapTime)))
+			ac.setText(l_lastlaptime, "Last Lap: {}".format(str(lastLapTime)))
+			ac.setFontColor(l_lastlaptime, 0, 0, 0, 1)
+			validLaps.append(lastLapTime)
+			ac.log("{} valid laps array".format(str(validLaps)))
+		else:
+			lastLapTime = 0
+			ac.log("Last Lap was invalid")
+			ac.setText(l_lastlaptime, "Last Lap: Invalid Lap")
+			ac.setFontColor(l_lastlaptime, 1, 0, 0, 1)
